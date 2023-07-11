9bf56ba04c445cf0260f24c5599facdf4c8f2e26
VishalModuguru <112614092+VishalModuguru@users.noreply.github.com>
2023-07-11 16:58:11 +0530

    Create initialising_grid_from_file.cpp

---
+++ examples/initialising_grid_from_file.cpp
@@ -0,0 +1,18 @@
+
+
+#include<iostream>
+#include"../src/sudoku_solver.h"
+
+/* NOTE: If you only need to utilise the Grid object,
+ * you can just import the "grid.h" header alone. */
+
+int main() {
+    sudoku::Grid grid;
+    grid.set_initial_state_from_file("samples/sample1.txt");
+    std::cout << grid << std::endl;
+
+    sudoku::solve(&grid);
+    std::cout << grid << std::endl;
+
+    return 0;
+}
